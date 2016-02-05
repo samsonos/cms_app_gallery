2f86abe016343156e631bcdd82c3dd94300e53da
Myslyvyi Dima <expenect@ukr.net>
2016-02-05 12:03:52 +0200

    added test

---
+++ tests/GalleryTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VITALYIEGOROV
+ * Date: 28.10.15
+ * Time: 11:28
+ */
+namespace samsoncms\app\gallery;
+
+
+class GalleryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGallery()
+    {
+        $this->assertEquals(true, true);
+    }
+}
